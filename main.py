35cff5806e62f842bdf1740afc08e2dcb7859282
Shashank Srivastav <shashankgrovy@gmail.com>
2014-10-29 00:49:34 +0530

    basic templates ready

--- main.py
+++ main.py
@@ -34,23 +34,34 @@ def teardown_request(exception):
 # views
 @app.route('/', methods=["GET"])
 def index():
+    #if session['logged_in']:
+        #return render_template('mainpage.html')
     return render_template('index.html')
 
-@app.route('/stocks.json', methods=["GET"])
-def get_stocks():
-    pass
 
 # authentication
-@app.route('/login', methods=['GET', 'SET'])
-def login():
-    if request.method == 'POST':
-        do_login()
-    else:
-        return render_template('login.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_user(request.form['username'] and request.form['password']):
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
 
 # get stocks data
-@app.route('/data')
+@app.route('/data.json', methods=["GET"])
 def get_data():
     BASEURL = "http://finance.yahoo.com/d/quotes.csv?s="
     PARAMS = "AAPL+GOOG+MSFT"
@@ -64,6 +75,12 @@ def get_data():
         data[sym] = {"name": name, "lo": lo, "hi": hi, "latest": latest}
     return jsonify(data)
 
+# 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     init_db()               # Setup the database
+    app.secret_key = 'supersecretkey'
     app.run(debug=True)
